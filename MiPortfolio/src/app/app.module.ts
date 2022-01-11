0d751986098de9b2f175d8040796da8b863d2dba
diegorus92 <diego_rus@outlook.com.ar>
2022-01-11 19:56:09 -0300

    actualizacion
    asdfklasdjflasdjasdfa
    }

--- MiPortfolio/src/app/app.module.ts
+++ MiPortfolio/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+
+const AppRoutes:Routes = [
+  { path: '', component:AppComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
